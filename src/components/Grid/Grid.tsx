aac0bbe4cbcddffce83553dd332e56378e6e292d
Victor Araujo <victor_emanuel9@hotmail.com>
2024-10-22 17:13:15 -0700

    feat: add grid-item-create style

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,5 +1,6 @@
 import "./Grid.style.css";
-import createPostImg from "../../assets/images/illustration-create-post.webp";
+import createPostImg from "@/assets/images/illustration-create-post.webp";
+import aiContentImg from "@/assets/images/illustration-ai-content.webp";
 
 export function Grid() {
   return (
@@ -7,19 +8,33 @@ export function Grid() {
       <main>
         <div className="grid-container">
           <div className="item grid-item-create">
-            <p>
-              Create and schedule content <span>quicker</span>
-            </p>
             <div>
-              <img src={createPostImg} alt="create post button" />
+              <p>
+                Create and schedule content <span>quicker</span>.
+              </p>
+              <div>
+                <img src={createPostImg} alt="create post" />
+              </div>
             </div>
           </div>
-          <div className="item grid-item-ai"></div>
+
+          <div className="item grid-item-ai">
+            <p>Write your content using Ai.</p>
+            <div>
+              <img src={aiContentImg} alt="ai talk" />
+            </div>
+          </div>
+
           <div className="item grid-item-social"></div>
+
           <div className="item grid-item-manage"></div>
+
           <div className="item grid-item-audience"></div>
+
           <div className="item grid-item-maintain"></div>
+
           <div className="item grid-item-grow"></div>
+
           <div className="item grid-item-schedule"></div>
         </div>
       </main>
