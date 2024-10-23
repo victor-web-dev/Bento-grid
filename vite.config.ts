f3cadaa0f6afdc7b2412c55668ead90476e45e6d
Victor Araujo <victor_emanuel9@hotmail.com>
2024-10-22 17:12:06 -0700

    setup: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
   server: {
     port: 3000,
   },
+  // resolve: {
+  //   alias: {
+  //     "@": __dirname + "./src",
+  //   },
+  // },
 });
